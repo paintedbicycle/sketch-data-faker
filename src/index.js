ec7c3ce76eb7ba780133517abe1f206ed3ffeb4f
Paul Wright <paul@paintedbicycle.com>
2018-11-14 23:26:52 +0100

    Fix layer name getting overridden. Add error messaging.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const sketch = require('sketch');
 const { DataSupplier } = sketch;
 const util = require('util');
 const faker = require('faker');
+const UI = require('sketch/ui');
 
 export function onStartup() {
   // To register the plugin, uncomment the relevant type:
@@ -78,36 +79,61 @@ export function onSupplyLoremParagraphs(context) {
 
 function supplyFakerData(context, type) {
   let dataKey = context.data.key;
+  let document = require('sketch/dom').getSelectedDocument();
   const items = util.toArray(context.data.items).map(sketch.fromNative);
   items.forEach((item, index) => {
     let data;
-    switch (type) {
-      case 'fullName':
-        data = faker.name.findName();
-        break;
-      case 'firstName':
-        data = faker.name.firstName();
-        break;
-      case 'lastName':
-        data = faker.name.lastName();
-        break;
-      case 'email':
-        data = faker.internet.email();
-        break;
-      case 'phoneNumber':
-        data = faker.phone.phoneNumber();
-        break;
-      case 'loremParagraph':
-        data = faker.lorem.paragraph();
-        break;
-      case 'loremParagraphs':
-        data = faker.lorem.paragraphs();
-        break;
-      default:
-        data = faker.fake('{{' + item.name + '}}');
-        break;
+    let custom;
+    let originalLayerName = item.name;
+    try {
+      switch (type) {
+        case 'fullName':
+          data = faker.name.findName();
+          break;
+        case 'firstName':
+          data = faker.name.firstName();
+          break;
+        case 'lastName':
+          data = faker.name.lastName();
+          break;
+        case 'email':
+          data = faker.internet.email();
+          break;
+        case 'phoneNumber':
+          data = faker.phone.phoneNumber();
+          break;
+        case 'loremParagraph':
+          data = faker.lorem.paragraph();
+          break;
+        case 'loremParagraphs':
+          data = faker.lorem.paragraphs();
+          break;
+        default:
+          // Set custom to true so we can override layer name
+          custom = true;
+          data = faker.fake('{{' + item.name + '}}');
+          console.log('data', data);
+          break;
+      }
+
+      // Replace the layer text
+      DataSupplier.supplyDataAtIndex(dataKey, data, index);
+
+      // To ensure we don't override the layer name
+      if (custom === true) {
+        let layer = document.getLayerWithID(item.id);
+        if (layer) {
+          // We now save the old layer name back
+          layer.name = originalLayerName;
+        }
+      }
+    } catch (e) {
+      UI.alert(
+        'Invalid Layer Name',
+        'A layer named "' +
+          originalLayerName +
+          '" returned no data. \n\nPlease double-check the layer name matches one of the data types.'
+      );
     }
-    if (!data) return;
-    DataSupplier.supplyDataAtIndex(dataKey, data, index);
   });
 }
