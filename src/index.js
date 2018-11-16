7f594762a9fef15e24ebcd6bb9ab470aa7783447
Paul Wright <paul@paintedbicycle.com>
2018-11-16 12:23:01 +0100

    Better error messaging

--- src/index.js
+++ src/index.js
@@ -80,60 +80,96 @@ export function onSupplyLoremParagraphs(context) {
 function supplyFakerData(context, type) {
   let dataKey = context.data.key;
   let document = require('sketch/dom').getSelectedDocument();
+  console.log('context.data', context.data);
   const items = util.toArray(context.data.items).map(sketch.fromNative);
+  let errors = [];
+
   items.forEach((item, index) => {
-    let data;
-    let custom;
+    let data = null;
+    let custom = false;
+    let layerError;
     let originalLayerName = item.name;
-    try {
-      switch (type) {
-        case 'fullName':
-          data = faker.name.findName();
-          break;
-        case 'firstName':
-          data = faker.name.firstName();
-          break;
-        case 'lastName':
-          data = faker.name.lastName();
-          break;
-        case 'email':
-          data = faker.internet.email();
-          break;
-        case 'phoneNumber':
-          data = faker.phone.phoneNumber();
-          break;
-        case 'loremParagraph':
-          data = faker.lorem.paragraph();
-          break;
-        case 'loremParagraphs':
-          data = faker.lorem.paragraphs();
-          break;
-        default:
-          // Set custom to true so we can override layer name
-          custom = true;
+    console.log('item', item);
+
+    switch (type) {
+      case 'fullName':
+        data = faker.name.findName();
+        break;
+      case 'firstName':
+        data = faker.name.firstName();
+        break;
+      case 'lastName':
+        data = faker.name.lastName();
+        break;
+      case 'email':
+        data = faker.internet.email();
+        break;
+      case 'phoneNumber':
+        data = faker.phone.phoneNumber();
+        break;
+      case 'loremParagraph':
+        data = faker.lorem.paragraph();
+        break;
+      case 'loremParagraphs':
+        data = faker.lorem.paragraphs();
+        break;
+      default:
+        // Set custom to true so we can override layer name
+        custom = true;
+        try {
           data = faker.fake('{{' + item.name + '}}');
-          console.log('data', data);
-          break;
-      }
-
-      // Replace the layer text
-      DataSupplier.supplyDataAtIndex(dataKey, data, index);
-
-      // To ensure we don't override the layer name
-      if (custom === true) {
-        let layer = document.getLayerWithID(item.id);
-        if (layer) {
-          // We now save the old layer name back
-          layer.name = originalLayerName;
+        } catch (e) {
+          layerError = true;
+          errors.push({
+            type: 'noData',
+            layer: {
+              name: originalLayerName
+            }
+          });
         }
+        break;
+    }
+    console.log('data', data);
+    console.log('originalLayerName', originalLayerName);
+
+    // If this specific layer has an error, do not continue
+    if (layerError) return;
+
+    // Replace the layer text
+    DataSupplier.supplyDataAtIndex(dataKey, data, index);
+
+    // To ensure we don't override the layer name
+    if (custom === 'bob') {
+      console.log('custom', custom);
+      let layer = document.getLayerWithID(item.id);
+      console.log('layer', layer);
+      if (layer) {
+        // We now save the old layer name back
+        layer.name = originalLayerName;
       }
-    } catch (e) {
-      UI.alert(
-        'Invalid Layer Name',
-        'A layer named "' +
-          originalLayerName +
-          '" returned no data. \n\nPlease double-check the layer name matches one of the data types.'
-      );
     }
   });
+
+  console.log('errors:', errors);
+  console.log('length:', errors.length);
+  if (errors.length === 1) {
+    UI.alert(
+      'Sketch Data Faker Error',
+      'A layer named "' +
+        errors[0].layer.name +
+        '" returned no data. \n\nPlease double-check the layer name matches one of the data types.'
+    );
+  } else if (errors.length > 1) {
+    function stringError() {
+      return errors
+        .map(error => {
+          return error.layer.name;
+        })
+        .join('\n');
+    }
+    UI.alert(
+      'Sketch Data Faker Error',
+      `There were some layers that returned no data. \n\nPlease double-check that the following layer names match one of the data types:\n\n${stringError()}`
+    );
+  }
 }
